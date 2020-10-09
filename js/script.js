8bee8f8103efcb69dbd342a13c88cf0ca256078e
Rafaelly Camargo <rafaelly.c@aluno.ifsp.edu.br>
2020-10-08 22:51:43 -0300

    Adicionei arquivo html e js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+    //declarando os valores do vetor
+    vetor = [85,1,67,32,99,41,3,19,56,10,87];
+
+    //função que ordena vetor
+    function sortFunction(x, y){
+        return (x-y);
+    }
+
+    //exibe informação
+    console.log (vetor.sort(sortFunction));
